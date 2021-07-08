85420874fa20a3fe96ab2d4dcaa4095e0731fb62
Taewoong1378 <ktw2378@korea.ac.kr>
2021-07-08 14:53:56 +0900

    third commit

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+$(function(){
+	$("#container").addClass("start");
+	$("nav li").on("click", function(){
+		$("#container").css("max-width", "100%")
+		const id=$(this).attr("data-rol");    
+		$("nav li").removeClass("on");    
+		$(this).addClass("on");    
+		$(".content").removeClass("prev this next");     
+		$("#" + id).addClass("this").prevAll().addClass("prev");    
+		$("#" + id).nextAll().addClass("next");    
+	});
+	// 로고 클릭 시
+	$(".logo_box").on("click", function(){
+		$("nav li").removeClass("on");
+		$(".content").removeClass("prev this next");
+		// $("#container").css("max-width", "1200px");
+	});
+});
